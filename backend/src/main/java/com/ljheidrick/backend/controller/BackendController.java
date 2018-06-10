e5c7eb1aa8a4950304e3d66375e26c54decf326b
root <root@DESKTOP-EQC1L1B.localdomain>
2018-06-10 10:43:53 -0500

    initial

---
+++ backend/src/main/java/com/ljheidrick/backend/controller/BackendController.java
@@ -0,0 +1,7 @@
+package com.ljheidrick.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class BackendController {
+}
