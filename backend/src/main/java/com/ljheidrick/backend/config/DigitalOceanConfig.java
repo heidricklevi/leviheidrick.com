cd3c1f1f78679cf021653188df270cbb79775160
Levi H <ljheidrick@gmail.com>
2018-09-09 08:05:11 -0500

    file storage/upload

---
+++ backend/src/main/java/com/ljheidrick/backend/config/DigitalOceanConfig.java
@@ -0,0 +1,34 @@
+package com.ljheidrick.backend.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DigitalOceanConfig {
+
+    @Value("${amazonProperties.endpointUrl}")
+    private String endpointUrl;
+    @Value("${amazonProperties.bucketName}")
+    private String bucketName;
+    @Value("${amazonProperties.accessKey}")
+    private String accessKey;
+    @Value("${amazonProperties.secretKey}")
+    private String secretKey;
+
+    private String DO_SPACES_REGION = "nyc3";
+
+    @Bean
+    public AmazonS3 getCredentials() {
+        BasicAWSCredentials creds = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpointUrl, DO_SPACES_REGION))
+                .withCredentials(new AWSStaticCredentialsProvider(creds)).build();
+    }
+
+}
--- backend/src/main/java/com/ljheidrick/backend/controller/ProjectsController.java
+++ backend/src/main/java/com/ljheidrick/backend/controller/ProjectsController.java
@@ -81,5 +81,4 @@ public class ProjectsController {
         projectsRepository.save(projects);
         return projectsRepository.findAll();
     }
-
 }
